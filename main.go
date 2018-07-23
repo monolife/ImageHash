59693f9715d78ca3889c7ffc3c68f29f9fd59bed
Amon Ducao <aducao@dzynetech.com>
2018-07-23 00:59:45 -0400

    Commenign out unused method for now

--- main.go
+++ main.go
@@ -36,22 +36,22 @@ type input struct {
 	FileName string
 }
 
-func GetFileContentType(out *os.File) (string, error) {
+// func GetFileContentType(out *os.File) (string, error) {
 
-	// Only the first 512 bytes are used to sniff the content type.
-	buffer := make([]byte, 512)
+// 	// Only the first 512 bytes are used to sniff the content type.
+// 	buffer := make([]byte, 512)
 
-	_, err := out.Read(buffer)
-	if err != nil {
-		return "", err
-	}
+// 	_, err := out.Read(buffer)
+// 	if err != nil {
+// 		return "", err
+// 	}
 
-	// Use the net/http package's handy DectectContentType function. Always returns a valid
-	// content-type by returning "application/octet-stream" if no others seemed to match.
-	contentType := http.DetectContentType(buffer)
+// 	// Use the net/http package's handy DectectContentType function. Always returns a valid
+// 	// content-type by returning "application/octet-stream" if no others seemed to match.
+// 	contentType := http.DetectContentType(buffer)
 
-	return contentType, nil
-}
+// 	return contentType, nil
+// }
 
 var (
 	useAll            = flag.Bool("all", false, "Compute all hashes")
